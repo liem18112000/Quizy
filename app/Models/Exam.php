3ca0c82d0fff14950d59d9b937fc7fc327612d7b
hoajoe2000 <45257672+hoajoe2000@users.noreply.github.com>
2020-10-18 11:01:15 +0700

    Implement Database 90%

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -1,11 +1,23 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\Console\Input\StringInput;
 
 class Exam extends Model
 {
+    public $table = "exams";
+    protected $guarded = [];
     use HasFactory;
+
+    public function createBy(){
+
+        return $this->belongsTo('App\Models\Teaching','user_id','user_id');
+    }
+
+    public function questions(){
+        return $this->hasMany('App\Models\Question',"exam_id",'id');
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('provider')->default('system');
-            $table->rememberToken();
+            // $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password');
+            // $table->string('provider')->default('system');
+            // $table->rememberToken();
             $table->timestamps();
         });
     }
