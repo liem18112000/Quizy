58604d38e1315d6bcfcddba05b9dff07f9a70ed9
hoajoe2000 <45257672+hoajoe2000@users.noreply.github.com>
2020-10-19 11:41:04 +0700

    Add user's choices DB

---
+++ database/migrations/2020_10_18_051843_create_exam_choice_log_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamChoiceLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exam_choice_log', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("course_id");
+            $table->foreignId("user_id");
+            $table->foreignId("role_type_id");
+            $table->foreignId("exam_id");
+            $table->foreignId("question_id");
+            $table->foreignId("choice_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exam_choice_log');
+    }
+}
