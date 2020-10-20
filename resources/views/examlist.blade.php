5b9ab88a3dd098f76a99f78576998d1d85b9ea40
Thư <1759038@itec.hcmus.edu.vn>
2020-10-21 00:33:28 +0700

    make datatable ver1

--- resources/views/examlist.blade.php
+++ resources/views/examlist.blade.php
@@ -1,438 +1,52 @@
 @extends('layouts.app')
+@section('styles')
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+@endsection
 
 @section('content')
-
- <!-- breadcrumb start-->
- <section class="breadcrumb breadcrumb_bg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="breadcrumb_iner text-center">
-                        <div class="breadcrumb_iner_item">
-                            <h2>Exam</h2>
-                            <p>Home<span>/</span>Exam</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- breadcrumb start-->
- <!-- feature_part start-->
- 
- <br />
-<div class="card">
-	<div class="card-header">
-		<div class="row">
-			<div class="col-md-9">
-				<h3 class="panel-title">Online Exam List</h3>
-			</div>
-			<div class="col-md-3" align="right">
-				<!-- <button type="button" id="add_button" class="btn btn-info btn-sm" data-toggle="modal" data-targe="formModal">Add</button> -->
-				
-
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalScrollable">
-  Launch demo modal
-</button>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-	  
-          			<h4 class="modal-title" id="question_modal_title"></h4>
-          			<button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-      
-      <div class="modal-body">
-	  <div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Question Title <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<input type="text" name="question_title" id="question_title" autocomplete="off" class="form-control" />
-	                		</div>
-            			</div>
-          			</div>
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Option 1 <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<input type="text" name="option_title_1" id="option_title_1" autocomplete="off" class="form-control" />
-	                		</div>
-            			</div>
-          			</div>
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Option 2 <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<input type="text" name="option_title_2" id="option_title_2" autocomplete="off" class="form-control" />
-	                		</div>
-            			</div>
-          			</div>
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Option 3 <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<input type="text" name="option_title_3" id="option_title_3" autocomplete="off" class="form-control" />
-	                		</div>
-            			</div>
-          			</div>
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Option 4 <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<input type="text" name="option_title_4" id="option_title_4" autocomplete="off" class="form-control" />
-	                		</div>
-            			</div>
-          			</div>
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Answer <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<select name="answer_option" id="answer_option" class="form-control">
-	                				<option value="">Select</option>
-	                				<option value="1">1 Option</option>
-	                				<option value="2">2 Option</option>
-	                				<option value="3">3 Option</option>
-	                				<option value="4">4 Option</option>
-	                			</select>
-	                		</div>
-            			</div>
-          			</div>
-        		</div>
-
-      
-      <div class="modal-footer">
-	  <input type="hidden" name="question_id" id="question_id" />
-
-<input type="hidden" name="online_exam_id" id="hidden_online_exam_id" />
-
-<input type="hidden" name="page" value="question" />
-
-<input type="hidden" name="action" id="hidden_action" value="Add" />
-
-<input type="submit" name="question_button_action" id="question_button_action" class="btn btn-success btn-sm" value="Add" />
-
-  <button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-<div class="modal" id="questionModal">
-  	<div class="modal-dialog modal-lg">
-    	<form method="post" id="question_form">
-      		<div class="modal-content">
-      			<!-- Modal Header -->
-        		<div class="modal-header">
-          			<h4 class="modal-title" id="question_modal_title"></h4>
-          			<button type="button" class="close" data-dismiss="modal">&times;</button>
-        		</div>
-
-        		<!-- Modal body -->
-        		<div class="modal-body">
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Question Title <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<input type="text" name="question_title" id="question_title" autocomplete="off" class="form-control" />
-	                		</div>
-            			</div>
-          			</div>
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Option 1 <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<input type="text" name="option_title_1" id="option_title_1" autocomplete="off" class="form-control" />
-	                		</div>
-            			</div>
-          			</div>
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Option 2 <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<input type="text" name="option_title_2" id="option_title_2" autocomplete="off" class="form-control" />
-	                		</div>
-            			</div>
-          			</div>
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Option 3 <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<input type="text" name="option_title_3" id="option_title_3" autocomplete="off" class="form-control" />
-	                		</div>
-            			</div>
-          			</div>
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Option 4 <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<input type="text" name="option_title_4" id="option_title_4" autocomplete="off" class="form-control" />
-	                		</div>
-            			</div>
-          			</div>
-          			<div class="form-group">
-            			<div class="row">
-              				<label class="col-md-4 text-right">Answer <span class="text-danger">*</span></label>
-	              			<div class="col-md-8">
-	                			<select name="answer_option" id="answer_option" class="form-control">
-	                				<option value="">Select</option>
-	                				<option value="1">1 Option</option>
-	                				<option value="2">2 Option</option>
-	                				<option value="3">3 Option</option>
-	                				<option value="4">4 Option</option>
-	                			</select>
-	                		</div>
-            			</div>
-          			</div>
-        		</div>
-
-	        	<!-- Modal footer -->
-	        	<div class="modal-footer">
-	        		<input type="hidden" name="question_id" id="question_id" />
-
-	        		<input type="hidden" name="online_exam_id" id="hidden_online_exam_id" />
-
-	        		<input type="hidden" name="page" value="question" />
-
-	        		<input type="hidden" name="action" id="hidden_action" value="Add" />
-
-	        		<input type="submit" name="question_button_action" id="question_button_action" class="btn btn-success btn-sm" value="Add" />
-
-	          		<button type="button" class="btn btn-danger btn-sm" data-dismiss="modal">Close</button>
-	        	</div>
-        	</div>
-    	</form>
-  	</div>
-</div>
-
-<script>
-
-$(document).ready(function(){
+<div class="container bootstrap snippet">
+    <div style='margin-top: 10%;'></div>
+    {{-- <div class="row">
+  		<div class="col-sm-10"><h1>User name</h1></div>
+    	<div class="col-sm-2"><a href="/users" class="pull-right"><img title="profile image" class="img-circle img-responsive" src="http://www.gravatar.com/avatar/28fd20ccec6865e2d5f0e1f4446eb7bf?s=100"></a></div>
+	</div> --}}
 	
-	var dataTable = $('#exam_data_table').DataTable({
-		"processing" : true,
-		"serverSide" : true,
-		"order" : [],
-		"ajax" : {
-			url: "ajax_action.php",
-			method:"POST",
-			data:{action:'fetch', page:'exam'}
-		},
-		"columnDefs":[
-			{
-				"targets":[7, 8, 9],
-				"orderable":false,
-			},
-		],
-	});
-   
+<section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <!-- Default box -->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Title</h3>
 
-	function reset_form()
-	{
-		$('#modal_title').text('Add Exam Details');
-		$('#button_action').val('Add');
-		$('#action').val('Add');
-		$('#exam_form')[0].reset();
-		$('#exam_form').parsley().reset();
-	}
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                    <i class="fas fa-minus"></i></button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                    <i class="fas fa-times"></i></button>
+                </div>
+              </div>
+              <div class="card-body">
+                Start creating your amazing application!
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer">
+                Footer
+              </div>
+              <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div>
+    </section>
 
-	$('#add_button').click(function(){
-		reset_form();
-        
-		$('#formModal').modal('show');
-		$('#message_operation').html('');
-	});
 
-	var date = new Date();
-
-	date.setDate(date.getDate());
-
-	$('#online_exam_datetime').datetimepicker({
-		startDate :date,
-		format: 'yyyy-mm-dd hh:ii',
-		autoclose:true
-	});
-
-	$('#exam_form').parsley();
-
-	$('#exam_form').on('submit', function(event){
-		event.preventDefault();
-
-		$('#online_exam_title').attr('required', 'required');
-
-		$('#online_exam_datetime').attr('required', 'required');
-
-		$('#online_exam_duration').attr('required', 'required');
-
-		$('#total_question').attr('required', 'required');
-
-		$('#marks_per_right_answer').attr('required', 'required');
-
-		$('#marks_per_wrong_answer').attr('required', 'required');
-
-		if($('#exam_form').parsley().validate())
-		{
-			$.ajax({
-				url:"ajax_action.php",
-				method:"POST",
-				data:$(this).serialize(),
-				dataType:"json",
-				beforeSend:function(){
-					$('#button_action').attr('disabled', 'disabled');
-					$('#button_action').val('Validate...');
-				},
-				success:function(data)
-				{
-					if(data.success)
-					{
-						$('#message_operation').html('<div class="alert alert-success">'+data.success+'</div>');
-
-						reset_form();
-
-						dataTable.ajax.reload();
-
-						$('#formModal').modal('hide');
-					}
-
-					$('#button_action').attr('disabled', false);
-
-					$('#button_action').val($('#action').val());
-				}
-			});
-		}
-	});
-
-	var exam_id = '';
-
-	$(document).on('click', '.edit', function(){
-		exam_id = $(this).attr('id');
-
-		reset_form();
-
-		$.ajax({
-			url:"ajax_action.php",
-			method:"POST",
-			data:{action:'edit_fetch', exam_id:exam_id, page:'exam'},
-			dataType:"json",
-			success:function(data)
-			{
-				$('#online_exam_title').val(data.online_exam_title);
-
-				$('#online_exam_datetime').val(data.online_exam_datetime);
-
-				$('#online_exam_duration').val(data.online_exam_duration);
-
-				$('#total_question').val(data.total_question);
-
-				$('#marks_per_right_answer').val(data.marks_per_right_answer);
-
-				$('#marks_per_wrong_answer').val(data.marks_per_wrong_answer);
-
-				$('#online_exam_id').val(exam_id);
-
-				$('#modal_title').text('Edit Exam Details');
-
-				$('#button_action').val('Edit');
-
-				$('#action').val('Edit');
-
-				$('#formModal').modal('show');
-			}
-		})
-	});
-
-	$(document).on('click', '.delete', function(){
-		exam_id = $(this).attr('id');
-		$('#deleteModal').modal('show');
-	});
-
-	$('#ok_button').click(function(){
-		$.ajax({
-			url:"ajax_action.php",
-			method:"POST",
-			data:{exam_id:exam_id, action:'delete', page:'exam'},
-			dataType:"json",
-			success:function(data)
-			{
-				$('#message_operation').html('<div class="alert alert-success">'+data.success+'</div>');
-				$('#deleteModal').modal('hide');
-				dataTable.ajax.reload();
-			}
-		})
-	});
-
-	function reset_question_form()
-	{
-		$('#question_modal_title').text('Add Question');
-		$('#question_button_action').val('Add');
-		$('#hidden_action').val('Add');
-		$('#question_form')[0].reset();
-		$('#question_form').parsley().reset();
-	}
-
-	$(document).on('click', '.add_question', function(){
-		reset_question_form();
-		$('#questionModal').modal('show');
-		$('#message_operation').html('');
-		exam_id = $(this).attr('id');
-		$('#hidden_online_exam_id').val(exam_id);
-	});
-
-	$('#question_form').parsley();
-
-	$('#question_form').on('submit', function(event){
-		event.preventDefault();
-
-		$('#question_title').attr('required', 'required');
-
-		$('#option_title_1').attr('required', 'required');
-
-		$('#option_title_2').attr('required', 'required');
-
-		$('#option_title_3').attr('required', 'required');
-
-		$('#option_title_4').attr('required', 'required');
-
-		$('#answer_option').attr('required', 'required');
-
-		if($('#question_form').parsley().validate())
-		{
-			$.ajax({
-				url:"ajax_action.php",
-				method:"POST",
-				data:$(this).serialize(),
-				dataType:"json",
-				beforeSend:function(){
-					$('#question_button_action').attr('disabled', 'disabled');
-
-					$('#question_button_action').val('Validate...');
-				},
-				success:function(data)
-				{
-					if(data.success)
-					{
-						$('#message_operation').html('<div class="alert alert-success">'+data.success+'</div>');
-
-						reset_question_form();
-						dataTable.ajax.reload();
-						$('#questionModal').modal('hide');
-					}
-
-					$('#question_button_action').attr('disabled', false);
-
-					$('#question_button_action').val($('#hidden_action').val());
-				}
-			});
-		}
-	});
-
-});
-
-</script>
-
+@endsection
+@section('scripts')
 
 
 
